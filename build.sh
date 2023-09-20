d8547bb42519adda2860a50919d204356ea1e1bb
Mikael Frykholm <mifr@sunet.se>
2023-09-20 16:07:10 +0200

    Rename.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 apt install -y openjdk-17-jdk-headless
 apt install -y maven
 mvn -version
+export DOCKER_REPO=local
 mvn clean install
 
-mvn -f bankid-idp/bankid-idp-backend jib:dockerBuild 
+mvn -f bankid-idp/bankid-idp jib:dockerBuild 
